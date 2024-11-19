2eb22eb3790a70b8dd5d4feb578dcfa8ca51e7f2
Michael Foss <michael.foss@mdpanel.com>
2024-11-19 18:25:51 -0500

    Add page hit counter

--- src/app/page.js
+++ src/app/page.js
@@ -24,10 +24,14 @@ writeFile(
 */
 
 function Home() {
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
+  hits++;
+  writeFile(DATABASE_PATH, JSON.stringify({ hits }));
+
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>You are visitor number {hits}.</p>
     </main>
   );
 }
